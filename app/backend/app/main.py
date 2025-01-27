3585ea67274825e356b3e44d6b2f7ae670929390
Paulo <pauloamino76@gmail.com>
2025-01-26 21:11:48 -0300

    feat(js): Adding Estoque page and list function

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -18,9 +18,15 @@ def get_config():
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
