74f89cb31c0deadb221c1ca6a25248ab869a99a5
Paulo <pauloamino76@gmail.com>
2025-02-06 22:44:25 -0300

    enchant(login-page): improving style from login page

--- app/adm-page/src/pages/AdminPage.tsx
+++ app/adm-page/src/pages/AdminPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import { loginUsuario } from '../services/api';  
-import Header from '../components/Header';
+import { useNavigate } from 'react-router-dom';
+import { loginUsuario } from '../services/api';
 import '../styles/loginStyle.css';
 
 const AdminPage = () => {
@@ -10,47 +9,45 @@ const AdminPage = () => {
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
-    event.preventDefault(); 
-  
+    event.preventDefault();
+
     try {
       const data = await loginUsuario(email, senha);
-      localStorage.setItem("access_token", data.access_token); 
-      navigate("/dashboard"); 
+      localStorage.setItem("access_token", data.access_token);
+      navigate("/dashboard");
     } catch (error) {
       alert("Email ou senha inválidos");
     }
   };
 
   return (
-    <div >
-      <Header></Header>
-      <div className="body">
-        <div className="container">
-          <div className="heading">Login</div>
-          <form action="" className="form">
-            <input
-              className="input"
-              type="email"
-              placeholder="E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              className="input"
-              type="password"
-              placeholder="Senha"
-              value={senha}
-              onChange={(e) => setSenha(e.target.value)}
-            />
-            <span className="forgot-password">
-              <a href="#">Esqueceu a senha ?</a>
-            </span>
-            <input className="login-button" type="submit" value="Sign In" onClick={handleLogin}/>
-          </form>
-          <span className="agreement">
-            <a href="">Learn user licence agreement</a>
+    <div className="body">
+      <div className="container">
+        <h1 className="admin-title">Página do Administrador</h1>
+        <div className="heading">Login</div>
+        <form className="form">
+          <input
+            className="input"
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="Senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          <span className="forgot-password">
+            <a href="#">Esqueceu a senha?</a>
           </span>
-        </div>
+          <input className="login-button" type="submit" value="Sign In" onClick={handleLogin} />
+        </form>
+        <span className="agreement">
+          <a href="">Learn user licence agreement</a>
+        </span>
       </div>
     </div>
   );
